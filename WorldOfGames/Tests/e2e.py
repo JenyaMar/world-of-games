696449c7623b19e2167ae49193bfc9cb187d2f7f
JenyaMart <evgmart1989@gmail.com>
2023-05-13 15:02:42 +0300

    deleted .idea folder

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -1,13 +1,12 @@
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def test_scores_service(url='http://127.0.0.1:5000'):
-    ch_driver = webdriver.Chrome(
-        executable_path='./chromedriver')
-    ch_driver.get(url)
-    score = ch_driver.find_element(By.ID, 'score')
+    driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
+    driver.get(url)
+    score = driver.find_element(By.ID, 'score')
     return True if (int(score.text) >= 1 and int(score.text) <= 1000) else False
 
 
