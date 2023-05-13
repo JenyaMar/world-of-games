102578c0d07182f153e4f9b53b17a9e23b497ac2
JenyaMart <evgmart1989@gmail.com>
2023-05-13 21:54:39 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -1,10 +1,12 @@
 import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def test_scores_service():
     driver = webdriver.Chrome('./chromedriver')
     driver.get('http://127.0.0.1:5000')
+    time.sleep(20)
     score = driver.find_element(By.ID, 'score')
     driver.quit()
     return True if (int(score.text) >= 1 and int(score.text) <= 1000) else False
