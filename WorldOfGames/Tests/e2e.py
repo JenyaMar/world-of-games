af327a638406af5e03f8c4583be86ed2d9cd51e4
JenyaMart <evgmart1989@gmail.com>
2023-05-20 18:00:40 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -2,10 +2,12 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 def test_scores_service():
-    driver = webdriver.Chrome('/chromedriver')
-    driver.get('https://www.google.com')
+    driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
+    driver.get('https://127.0.0.1:5000/')
     time.sleep(20)
     score = driver.find_element(By.ID, 'score')
     driver.quit()
