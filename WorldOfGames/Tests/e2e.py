f0f29c2a6706aa605007f5c54b722bd1ebea57de
JenyaMart <evgmart1989@gmail.com>
2023-05-20 21:58:57 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.options import Options
 
 def scores_service():
     chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Chrome(executable_path="/Users/jenyamartemyanov/Downloads/chromedriver_mac64/chromedriver.exe", options=chrome_options)
     driver.get('http://172.0.0.1:5001')
     time.sleep(5)
