261a1e793071ea24963cb720e9c736a6e4be3c97
JenyaMart <evgmart1989@gmail.com>
2023-05-20 21:36:30 +0300

    first commit

--- WorldOfGames/MainScores.py
+++ WorldOfGames/MainScores.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def score_server():
-    try:
-        with open(f'./{SCORES_FILE_NAME}', "r") as f:
-            content = f.read()
-            return render_template("score.html", score=content)
-    except FileNotFoundError as e:
-        return render_template("error.html", error=e)
+    return "<p>Hello, World!</p>"
+    # try:
+    #     with open(SCORES_FILE_NAME, "r") as f:
+    #         content = f.read()
+    #         return render_template("score.html", score=content)
+    # except FileNotFoundError as e:
+    #     return render_template("error.html", error=e)
 
 
 if __name__ == '__main__':
