355ea28ce14bd26df66d3129f9dcabaee4d9cd69
JenyaMart <evgmart1989@gmail.com>
2023-05-20 19:13:16 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -2,9 +2,12 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 def test_scores_service():
-    driver = webdriver.Chrome('/chromedriver')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path="/Users/jenyamartemyanov/Downloads/chromedriver_mac64/chromedriver.exe", options=chrome_options)
     driver.get('127.0.0.1:5000')
     time.sleep(20)
     score = driver.find_element(By.ID, 'score')
