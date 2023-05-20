693ecacb88ff96fb6abcd5c7048935c68b221a88
JenyaMart <evgmart1989@gmail.com>
2023-05-20 21:27:33 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -6,11 +6,8 @@ from selenium.webdriver.chrome.options import Options
 
 def scores_service():
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Chrome(executable_path="/Users/jenyamartemyanov/Downloads/chromedriver_mac64/chromedriver.exe", options=chrome_options)
-    driver.get('http://127.0.0.1:5000')
+    driver.get('http://127.0.0.1:5001')
     time.sleep(5)
     score = driver.find_element(By.ID, 'score')
     return True if (int(score.text) >= 1 and int(score.text) <= 1000) else False
