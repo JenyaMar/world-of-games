337471f3f0d408a2a3bf9a0c21d75f2088891340
JenyaMart <evgmart1989@gmail.com>
2023-05-20 21:39:26 +0300

    first commit

--- WorldOfGames/Tests/e2e.py
+++ WorldOfGames/Tests/e2e.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def scores_service():
     chrome_options = Options()
     driver = webdriver.Chrome(executable_path="/Users/jenyamartemyanov/Downloads/chromedriver_mac64/chromedriver.exe", options=chrome_options)
-    driver.get('http://127.0.0.1:5001')
+    driver.get('http://0.0.0.0:5001')
     time.sleep(5)
     score = driver.find_element(By.ID, 'score')
     return True if (int(score.text) >= 1 and int(score.text) <= 1000) else False
